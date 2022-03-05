5df2afd99e95dc177eec2c24eadae948fbae4391
Mohamed Hassan <mohassan787898@gmail.com>
2022-03-05 16:35:09 +0200

    add components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import Navbar  from './components/Navbar.jsx';
+import Banner  from './components/Banner.jsx';
+import Carousel  from './components/Carousel.jsx';
+import Company   from './components/Company.jsx';
+import Footer    from './components/Footer.jsx';
+
+
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hi Mo </h1>
+    <div className="bg-neutral-100 dark:bg-gray-800 text-gray-800 dark:text-white">
+      <Navbar />
+      <Banner />
+      <div>
+        <Carousel />
+        <Company />
+      </div>
+      <Footer />
     </div>
   );
 }

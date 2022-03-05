df6429bf37c731cf4d0217c98f7eb9686a9132ed
Mohamed Hassan <mohassan787898@gmail.com>
2022-03-05 17:12:20 +0200

    add Logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,8 @@
+export const Logo = () => {
+    return (
+        <span className="bg-gradient-to-r from-fuchsia-800 via-purple-600 to-indigo-800 bg-clip-text align-middle text-2xl font-black tracking-normal text-transparent ">
+            HIRE ME
+        </span>
+    );
+}
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Logo } from './Logo'
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+      <header className='sticky inset-x-0 top-0 z-20 flex items-center justify-between bg-white py-2 px-2 shadow-lg dark:bg-slate-900'>
+        <div className='flex grow-0 basis-1/3'>Menu btn</div>
+        <div className='flex grow-0 basis-1/3 justify-center'><Logo /></div>
+        <div className='flex grow-0 basis-1/3 justify-end'><Theme btn></div>
+      </header>
+    </>
   )
 }
 
